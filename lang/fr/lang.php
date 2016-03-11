6b9884b7fae8f2e00f6091b7a0a1a133f20b7086
Laynee <seedfloyd@gmail.com>
2016-03-12 00:09:58 +0100

    French language file

---
+++ lang/fr/lang.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * French language file for newsticker plugin
+ *
+ * @author Laynee <seedfloyd@gmail.com>
+ */
+
+// menu entry for admin plugins
+// $lang['menu'] = 'Your menu entry';
+
+// custom language strings for the plugin
+$lang['previous'] = 'Précédent';
+$lang['next'] = 'Suivant';
+
+
+
+//Setup VIM: ex: et ts=4 :
