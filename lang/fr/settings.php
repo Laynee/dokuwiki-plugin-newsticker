9abc0e2e1c76807cc92eccf880e82d7bc3b11d6d
Laynee <seedfloyd@gmail.com>
2016-03-12 00:11:15 +0100

    French language file

---
+++ lang/fr/settings.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * French language file for newsticker plugin
+ *
+ * @author Laynee <seedfloyd@gmail.com>
+ */
+
+// keys need to match the config setting name
+$lang['duration'] = 'Durée d\'affichage de chaque message (en secondes).';
+
+
+
+//Setup VIM: ex: et ts=4 :
